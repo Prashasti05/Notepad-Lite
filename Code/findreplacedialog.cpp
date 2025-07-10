a9806964e4f9dfb9a50a98b4b4c22997eb910932
Prashasti <paulprashasti@gmail.com>
2025-07-11 02:16:58 +0530

    Update findreplacedialog.cpp

--- Code/findreplacedialog.cpp
+++ Code/findreplacedialog.cpp
@@ -2,10 +2,10 @@
 #include "ui_findreplacedialog.h"
 
 FindReplaceDialog::FindReplaceDialog(QWidget *parent)
-    : QDialog(parent)//constrctor initializes baseclass QDialog
-    , ui(new Ui::FindReplaceDialog) //nd allocates memorry for ui and sets it up
+    : QDialog(parent)
+    , ui(new Ui::FindReplaceDialog) 
 {
-    ui->setupUi(this); //Initializes UI elements from the .ui file
+    ui->setupUi(this);
 }
 
 FindReplaceDialog::~FindReplaceDialog()
@@ -13,19 +13,15 @@ FindReplaceDialog::~FindReplaceDialog()
     delete ui;
 }
 
-//find and replace-----------------------------------------
 void FindReplaceDialog::on_btnFind_clicked()
 {
     emit findNext(ui->lineEditFind->text());
-    //emits findnext signal for(retrieves find text input by user in lineedit)
 }
 
 void FindReplaceDialog::on_btnReplace_clicked()
 {
     emit replace(ui->lineEditFind->text(),ui->lineEditReplace->text());
-    //emits replace signal when both find text and replacement text retrived.
 }
-
 
 void FindReplaceDialog::on_btnReplaceAll_clicked()
 {
